053ca547fc4429e046ea74bef991329c2aa916f3
Sari Nusier <sari@sqhive.london>
2017-04-12 11:40:07 +0100

    Code for question1 almost done. Get back later.

--- DM/Assignment2/code/question1.py
+++ DM/Assignment2/code/question1.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 DEFAULT_PATH_NAME = '../data/lastfm.csv'
@@ -9,29 +8,38 @@ def read_file(pathname):
         return pd.read_csv(pathname)
     except IOError as ioe:
         """
-        Exit program? Read execption handling for this case.
+        Exit program? Read exception handling for this case.
         """
-        print ioe
+        print(ioe)
 
 
 def find_rule_in_df(data_frame, l, r):
-    # instances_with_l = data_frame.any('artist' == l)
     users_with_rule = []
     grouped_by_user = data_frame.groupby('user')['artist'].apply(list)
     for u in grouped_by_user.index.values:
         if {l, r}.issubset(grouped_by_user[u]):
             users_with_rule.append(u)
-    return users_with_rule
-    # containing_L_R = grouped_by_user[(grouped_by_user['artist'].any(l)) & (grouped_by_user['artist'].any(r))]
-    # print grouped_by_user
+    return users_with_rule, len(users_with_rule)
 
 
 def main():
     data_frame = read_file(DEFAULT_PATH_NAME)
-    top_three_freq = data_frame["artist"].value_counts()[:3]
-    a = data_frame.groupby(['user', 'artist'])['artist'].count()
-    rules = find_rule_in_df(data_frame, 'red hot chili peppers', 'goldfrapp')
-    print rules
+    one_item_sets_coverage = data_frame["artist"].value_counts()
+    top_three_freq = one_item_sets_coverage[:3]
+    a = top_three_freq.index.values[0]
+    b = top_three_freq.index.values[1]
+    c = top_three_freq.index.values[2]
+    print(str("Total transactions: " + str(len(data_frame['user'].unique()))))
+    print(a)
+    print(b)
+    print(c)
+    _, support2 = find_rule_in_df(data_frame, 'goldfrapp', 'red hot chili peppers')
+    _, ab = find_rule_in_df(data_frame, a, b)
+    _, ac = find_rule_in_df(data_frame, a, c)
+    _, ba = find_rule_in_df(data_frame, b, a)
+    print(ab)
+    print(ac)
+    print(ba)
 
 
 if __name__ == "__main__":
