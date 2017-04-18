a3eb25ee328ee29061ec9ceb5ecfdbe3c3f4b49c
Sari Nusier <sari@sqhive.london>
2017-04-19 00:55:21 +0100

    Finished QLearning assignment for ML

--- ML/Assignment2/code/reinforcement/mlLearningAgents.py
+++ ML/Assignment2/code/reinforcement/mlLearningAgents.py
@@ -23,18 +23,131 @@
 # pacmanAgents.py
 # learningAgents.py
 
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
 from pacman import Directions
 from game import Agent
 import random
-import game
-import util
+
+
+class StateAsKey:
+    """ 
+    Helper class. Used as a key in a dictionary.
+    The hashing and comparison functions are overriden in order to use the objects as keys in a dict.
+    """
+
+    def __init__(self, state, action):
+        self.pacpos = state.getPacmanPosition()
+        self.ghostpos = frozenset(state.getGhostPositions())
+        self.food = str(state.getFood())
+        self.action = action
+
+    def __hash__(self):
+        return hash((self.pacpos, self.ghostpos, self.food, self.action))
+
+    def __eq__(self, other):
+        return (self.pacpos, self.ghostpos, self.food, self.action) == (
+            other.pacpos, other.ghostpos, other.food, other.action)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class QLearner:
+    """
+    This is the learner class.
+    The methods' description are written below.
+    """
+    def __init__(self, alpha, epsilon, gamma):
+        self.state_to_update_key = None
+        self.state_to_update = None
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.alpha = alpha
+        self.q_values = {}
+
+    def decide(self, state, is_final=False):
+        """
+        Returns an action and updates q-values. 
+        For simplicity, the same function is called for terminal states as well, 
+        with the by setting the is_final flag to True 
+        """
+        if self.state_to_update_key is not None:
+            self.q_values[self.state_to_update_key] = self.compute_bellman(state)
+
+        if not is_final:
+            action = self.get_action(state)
+            self.state_to_update_key = StateAsKey(state, action)
+            self.state_to_update = state
+            if not (self.state_to_update_key in self.q_values):
+                self.q_values[self.state_to_update_key] = 0
+            return action
+
+        self.state_to_update_key = None
+        self.q_values[StateAsKey(state, None)] = state.getScore()
+        # We return None when the state is terminal (no action is needed)
+        return None
+
+    def compute_bellman(self, new_state):
+        """
+        Computes Q(s,a) when moving from s -> s'
+        :param new_state: This is s' 
+        :return: The new value of Q(s,a) 
+        """
+        prev_state = self.state_to_update
+        # cur_q represents the current Q value of the state to be updated (prev_state): Q(s,a)
+        cur_q = self.q_values[self.state_to_update_key]
+        update_val = self.alpha * (prev_state.getScore() + self.gamma * self.get_max_Q(new_state)[1] - cur_q)
+        return cur_q + update_val
+
+    def get_action(self, state):
+        """
+        Chooses an action using epsilon greedy.
+        :param state: State containing all the valid moves 
+        :return: Returns a valid move for the state 
+        """
+        legal = state.getLegalPacmanActions()
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+
+        if random.random() > self.epsilon:
+            mx_q = self.get_max_Q(state)[0]
+            return mx_q
+        return random.choice(legal)
+
+    def get_max_Q(self, state):
+        """
+        Finds the maximum Q value for the state and all the valid actions in that state.
+        It returns both the action(argmax) and the maximum value.
+        :param state: State we are interested in.
+        :return: Returns argmax and the maximum value
+        """
+        legal = state.getLegalPacmanActions()
+        if Directions.STOP in legal:
+            legal.remove(Directions.STOP)
+
+        # When the state is terminal, there are no legal moves.
+        if len(legal) == 0:
+            return None, state.getScore()
+
+        for action in legal:
+            if not (StateAsKey(state, action) in self.q_values):
+                self.q_values[StateAsKey(state, action)] = 0
+
+        all_vals = [self.q_values[StateAsKey(state, action)] for action in legal]
+        max_val = max(all_vals)
+        arg_max = legal[all_vals.index(max_val)]
+
+        return arg_max, max_val
+
 
 # QLearnAgent
 #
 class QLearnAgent(Agent):
 
     # Constructor, called when we start running the
-    def __init__(self, alpha=0.2, epsilon=0.05, gamma=0.8, numTraining = 10):
+    def __init__(self, alpha=0.3, epsilon=0.2, gamma=0.8, numTraining=10):
         # alpha       - learning rate
         # epsilon     - exploration rate
         # gamma       - discount factor
@@ -49,17 +162,17 @@ class QLearnAgent(Agent):
         self.numTraining = int(numTraining)
         # Count the number of games we have played
         self.episodesSoFar = 0
+        self.q_learner = QLearner(self.alpha, self.epsilon, self.gamma)
 
-    
     # Accessor functions for the variable episodesSoFars controlling learning
     def incrementEpisodesSoFar(self):
-        self.episodesSoFar +=1
+        self.episodesSoFar += 1
 
     def getEpisodesSoFar(self):
         return self.episodesSoFar
 
     def getNumTraining(self):
-            return self.numTraining
+        return self.numTraining
 
     # Accessor functions for parameters
     def setEpsilon(self, value):
@@ -70,54 +183,37 @@ class QLearnAgent(Agent):
 
     def setAlpha(self, value):
         self.alpha = value
-        
+
     def getGamma(self):
         return self.gamma
 
     def getMaxAttempts(self):
         return self.maxAttempts
 
-    
     # getAction
     #
     # The main method required by the game. Called every time that
     # Pacman is expected to move
     def getAction(self, state):
-
-        # The data we have about the state of the game
-        legal = state.getLegalPacmanActions()
-        if Directions.STOP in legal:
-            legal.remove(Directions.STOP)
-        print "Legal moves: ", legal
-        print "Pacman position: ", state.getPacmanPosition()
-        print "Ghost positions:" , state.getGhostPositions()
-        print "Food locations: "
-        print state.getFood()
-        print "Score: ", state.getScore()
- 
-            
-        # Now pick what action to take. For now a random choice among
-        # the legal moves
-        pick = random.choice(legal)
-        # We have to return an action
-        return pick
-            
+        return self.q_learner.decide(state)
 
     # Handle the end of episodes
     #
     # This is called by the game after a win or a loss.
     def final(self, state):
 
-        print "A game just ended!"
-        
+        print("A game just ended!")
+
+        self.q_learner.decide(state, is_final=True)
         # Keep track of the number of games played, and set learning
         # parameters to zero when we are done with the pre-set number
         # of training episodes
         self.incrementEpisodesSoFar()
+        print(self.getEpisodesSoFar())
         if self.getEpisodesSoFar() == self.getNumTraining():
             msg = 'Training Done (turning off epsilon and alpha)'
-            print '%s\n%s' % (msg,'-' * len(msg))
+            print('%s\n%s' % (msg, '-' * len(msg)))
             self.setAlpha(0)
             self.setEpsilon(0)
-
-
+            self.q_learner.alpha = 0
+            self.q_learner.epsilon = 0
